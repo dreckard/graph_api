cf9f94bdc4885198545be4e0e90b0297f24c926b
dreckard <dreckard@users.noreply.github.com>
2015-03-21 13:05:14 +0800

    Initial commit

---
+++ graph_api.js
@@ -0,0 +1,126 @@
+// --------------------------------------------------------------------------------
+// Simple throttling Facebook graph API library for node.js
+// 
+// Install/Setup:
+//   Simply place in the proper directory and load as a file module with
+//   require('./graph_api')
+//
+// Usage:
+//   All requests return Q promise objects and errors are logged to <logFile>
+//   
+//   get(path):
+//   delete(path):
+//   post(path,content):
+//   put(path,content):
+//   These functions all issue the HTTP requests their names suggest to the
+//   URL at <exports.endpoint> + <path> using the form data specified in <content>
+//   
+//   Requests are throttled to <exports.throttleRequests> per 
+//   <exports.throttleInterval> sec and will be queued if the limit is exceeded
+//
+// dreckard
+// March 2015
+// --------------------------------------------------------------------------------
+var url = require('url');
+var assert = require('assert');
+var request = require('request');
+var Q = require('q');
+var winston = require('winston');
+var Queue = require('tiny-queue');
+
+var throttleCounter = 0;
+var throttleQueue = new Queue(); //This can be done with a plain old array but the shifting will be slow
+
+exports.throttleRequests = 60;
+exports.throttleInterval = 600; //sec
+exports.logFile = 'request_log.txt';
+exports.endpoint = null;
+exports.token = null;
+
+//Setup logging
+winston.add(winston.transports.File, { filename: exports.logFile });
+
+function checkRequestError(requestType,path,response,body,error)
+{
+    var errMsg = requestType + ' ' + path + ' failed; ';
+    if ( error )
+    {
+        winston.error(errMsg + error.message);
+        return error;
+    }
+    
+    try
+    {
+        var resp = JSON.parse(body);
+        if ( resp.error )
+        {
+            winston.error(errMsg + resp.error.type + '; ' + resp.error.message);
+            return new Error(errMsg + resp.error.type + '; ' + resp.error.message);
+        }
+    }
+    catch (e)
+    { 
+        winston.error(errMsg + e.message);
+        return new Error(errMsg + e.message); 
+    }
+    return null;
+}
+
+function genUrl(path)
+{
+    var request_url = url.parse(url.resolve(exports.endpoint,path),true);
+    request_url.query.access_token = exports.token;
+    return request_url;
+}
+function submitRequest(context)
+{
+    //DEBUG
+    //winston.info('Submitting request to: ' + context.url);
+    //context.deferred.resolve('Result!\nCounter: ' + throttleCounter + '\nQueue: ' + throttleQueue.length);
+    
+    request({method: context.method, url: context.url, form: context.form}, function (error, response, body) {
+        var reqError = checkRequestError(context.method,context.url,response,body,error);
+        if (reqError)
+            context.deferred.reject(reqError);
+        else
+            context.deferred.resolve(body);
+        });
+}
+function processRequestQueue() //Called every time a throttled request "ages out" (i.e. after throttleInterval secs) 
+{
+    assert(throttleCounter > 0, 'throttleCounter > 0 failed');
+    
+    if ( throttleQueue.length > 0 )
+        submitRequest(throttleQueue.shift());
+    throttleCounter--;
+}
+function queueRequest(reqMethod,path,content)
+{
+    var def = Q.defer();
+    var reqContext = {deferred: def, method: reqMethod, url: url.format(genUrl(path)), form: content};
+    
+    throttleCounter++;
+    if ( throttleCounter >= exports.throttleRequests )
+        throttleQueue.push(reqContext);
+    else
+        submitRequest(reqContext);
+    
+    setTimeout(processRequestQueue,exports.throttleInterval*1000);
+    return def.promise;
+}
+exports.get = function(path)
+{
+    return queueRequest('GET',path);
+}
+exports.post = function(path,content)
+{
+    return queueRequest('POST',path,content);
+}
+exports.put = function(path,content)
+{
+    return queueRequest('PUT',path,content);
+}
+exports.delete = function(path)
+{
+    return queueRequest('DELETE',path);
+}
---
+++ testserver.js
@@ -0,0 +1,49 @@
+// --------------------------------------------------------------------------------
+// Throttling Facebook Graph API library - simple node server for testing
+// 
+// Implements routes for /get, /post, and /delete/:id to test the API
+// Your own token is required, of course
+//
+// dreckard
+// March 2015
+// --------------------------------------------------------------------------------
+//http://localhost:3000/api/stats?ad_ids=1,2,3&start_time=2013-09-01&end_time=2013-10-01
+var url = require('url');
+var express = require('express');
+var api = require('./graph_api');
+
+api.throttleRequests = 10;
+api.throttleInterval = 10; //sec
+api.endpoint = 'https://graph.facebook.com';
+api.token = 'token';
+
+var app = express();
+var server = app.listen(3000);
+app.get('/', function (req,res) {
+        res.end('Server is running...');
+    });
+app.get('/get', function (req,res) {
+        api.get('/me')
+        .done( function(body) { res.end(body); },
+               function(err)  { res.end('Error: ' + err.message) } );
+    });
+app.get('/post', function (req,res) {
+        api.post('/me/feed',{message: 'Hello Graph API'})
+        .done( function(body) { res.end(body); },
+               function(err)  { res.end('Error: ' + err.message) } );
+    });
+app.get('/delete/:id', function (req,res) {
+        var pars = url.parse(req.url,true);
+        api.delete('/'+req.params.id)
+        .done( function(body) { res.end(body); },
+               function(err)  { res.end('Error: ' + err.message) } );
+    });
+    
+//The FB API docs don't list any PUT endpoints but here it is for completeness anyway
+/*app.get('/put', function (req,res) {
+        api.put('/me/feed',{message: 'Hello Graph API'})
+        .done( function(body) { res.end(body); },
+               function(err)  { res.end('Error: ' + err.message) } );
+    });*/
+    
+console.log('Server running...');
